2c91d27ea6275a28a1d8925dc4686db097069a5f
Folkert <folkert.brokking@gmail.com>
2026-03-24 09:47:27 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StockValuator from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StockValuator />
+  </React.StrictMode>,
+)
