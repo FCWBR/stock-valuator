a93e367071378ffdc4b75f7265eab9fa5e08ad1b
Folkert <folkert.brokking@gmail.com>
2026-03-24 10:22:32 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // For GitHub Pages: replace 'stock-valuator' with your repo name
-  base: '/stock-valuator/',
-})
