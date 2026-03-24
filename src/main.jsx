6614cb63006bc50ef3132ca1a9be9ff3cd385422
Folkert <folkert.brokking@gmail.com>
2026-03-24 10:21:32 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import StockValuator from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <StockValuator />
-  </React.StrictMode>,
-)
